e05cfe413cd4e145e451ea2712c5ab6bab4ba32e
Yoimer <ydrf19@gmail.com>
2018-06-17 20:14:09 -0400

    added app3.js and EVENTAPP as a single global object

---
+++ scripts/app3.js
@@ -0,0 +1,67 @@
+// the variables and functions scope are not longer global
+var EVENTAPP = (function(){
+    var content = document.querySelector("#reporter"),
+        listeners = [],
+        listenersEnabled = false,
+        printIt = true,
+        lastevent;
+
+    // write info to the div. includes event object information
+    var loadInfo = function(message, eventObj) {
+        content.insertAdjacentHTML("afterbegin", message + " -- event type: " + 
+        eventObj.type + " -- target object: " + eventObj.target.nodeName + "<br>");
+    };
+
+    // returning an object to make them accessible from outside
+    return {
+        // adds listenets to the document
+        addListeners: function() {
+            var keyDownHandler = function (e) {
+                loadInfo("A key was pressed: " + e.keyCode + " -- " + e.key, e);
+                if (e.keyCode === 83 && e.ctrlKey) {
+                    toggleEventListeners();
+                }
+            };
+            document.addEventListener("keydown", keyDownHandler);
+            listeners.push(keyDownHandler, "keydown");
+
+            var mouseMoveHandler = function(e){
+                lastevent = e;
+                if (printIt) {
+                    printIt = false;
+                    loadInfo("Mouse move recorded at coordinates: " + e.pageX + ", " + e.pageY, e);
+                    setTimeout(function() {
+                        printIt = true;
+                    }, 500);
+                }
+            };
+            document.addEventListener("mousemove", mouseMoveHandler);
+            listeners.push(mouseMoveHandler, "mousemove");
+
+            // once listeners are added. sets this to true for toggle function
+            listenersEnabled = true;
+        },
+
+        // removes listeners from document so user can examine data
+        removeListeners: function(){
+            while (listeners.length > 0) {
+                document.removeEventListener(listeners.pop(), listeners.pop());
+            }
+        },
+
+        //called to initialize, determines whether to add or remove listeners based
+        //on current state
+        toggleEventListeners: function() {
+            if (listenersEnabled) {
+                this.removeListeners();
+                console.log("Event listeners removed");
+            } else {
+                this.addListeners();
+                console.log("listeners Added");
+            }
+        }
+    };
+})();
+
+// sets up listeners
+EVENTAPP.toggleEventListeners();
